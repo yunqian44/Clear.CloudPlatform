8ee7b74aa6fc282ba8b1ed195644d512ea22267a
yunqian44 <yunqian8@live.com>
2022-03-03 13:57:29 +0000

    Add ToolsBlock Model

---
+++ src/Domain/Entities/ToolsBlockEntities/CommentReply.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clear.CloudPlatform.Domain.Entities.ToolsBlockEntities;
+
+public class CommentReply
+{
+    public Guid Id { get; set; }
+
+    public Guid CommentId { get; set; }
+
+    public string? CommentContent { get; set; }
+
+    public string? Title { get; set; }
+
+    public DateTime CreateTimeUtc { get; set; }
+}
---
+++ src/Domain/Entities/ToolsBlockEntities/ToolsBlockItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clear.CloudPlatform.Domain.Entities.ToolsBlockEntities;
+
+public class ToolsBlockItem
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Version { get; set; }
+
+    public string? Url { get; set; }
+
+    public DateTime CreateTimeUtc { get; set; }
+
+    public string? CommentContent { get; set; }
+
+    public IReadOnlyList<CommentReply> CommentReplies { get; set; }
+}
