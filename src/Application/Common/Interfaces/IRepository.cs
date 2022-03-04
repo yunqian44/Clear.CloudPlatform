8ed56aa402350636ca6b8dd9700d41414506c649
yunqian44 <yunqian8@live.com>
2022-03-04 06:45:10 +0000

    Add IRepo

---
+++ src/Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clear.CloudPlatform.Application.Common.Interfaces;
+
+public interface IRepository<T> //where T : class
+{
+    Task ExecuteSqlRawAsync(string sql);
+
+    ValueTask<T> GetAsync(object key);
+
+    Task<T> GetAsync(Expression<Func<T, bool>> condition);
+
+    Task<IReadOnlyList<T>> GetAsync();
+
+    Task<IReadOnlyList<T>> GetAsync(ISpecification<T> spec);
+
+    IQueryable<T> GetAsQueryable();
+
+    TResult SelectFirstOrDefault<TResult>(
+        ISpecification<T> spec,
+        Expression<Func<T, TResult>> selector);
+
+    Task DeleteAsync(T entity);
+
+    Task DeleteAsync(IEnumerable<T> entities);
+
+    Task DeleteAsync(object key);
+
+    int Count(ISpecification<T> spec = null);
+
+    int Count(Expression<Func<T, bool>> condition);
+
+    Task<int> CountAsync(ISpecification<T> spec);
+
+    bool Any(ISpecification<T> spec);
+
+    bool Any(Expression<Func<T, bool>> condition = null);
+
+    Task<IReadOnlyList<TResult>> SelectAsync<TResult>(
+        Expression<Func<T, TResult>> selector);
+
+    Task<IReadOnlyList<TResult>> SelectAsync<TResult>(
+        ISpecification<T> spec,
+        Expression<Func<T, TResult>> selector);
+
+    Task<TResult> SelectFirstOrDefaultAsync<TResult>(
+        ISpecification<T> spec,
+        Expression<Func<T, TResult>> selector);
+
+    Task<IReadOnlyList<TResult>> SelectAsync<TGroup, TResult>(
+        Expression<Func<T, TGroup>> groupExpression,
+        Expression<Func<IGrouping<TGroup, T>, TResult>> selector,
+        ISpecification<T> spec = null);
+
+    Task<T> AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+}
