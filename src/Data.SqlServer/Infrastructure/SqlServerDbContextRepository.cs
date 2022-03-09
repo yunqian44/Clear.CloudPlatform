7b4aaa016487d74586cdc75528174732c4598efa
yunqian44 <yunqian8@live.com>
2022-03-09 15:42:06 +0000

    Add SqlServer DBContext

---
+++ src/Data.SqlServer/Infrastructure/SqlServerDbContextRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clear.CloudPlatform.Data.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clear.CloudPlatform.Data.SqlServer.Infrastructure;
+
+[ExcludeFromCodeCoverage]
+public class SqlServerDbContextRepository<T> : DbContextRepository<T> where T : class
+{
+    public SqlServerDbContextRepository(SqlServerDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+
+    public override async Task ExecuteSqlRawAsync(string sql)
+    {
+        await DbContext.Database.ExecuteSqlRawAsync(sql);
+    }
+}
---
+++ src/Data.SqlServer/SqlServerDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clear.CloudPlatform.Data.SqlServer;
+
+[ExcludeFromCodeCoverage]
+public class SqlServerDbContext : ToolsBlockDbContext
+{
+    public SqlServerDbContext()
+    {
+    }
+
+    public SqlServerDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new CommentConfiguration());
+        //modelBuilder.ApplyConfiguration(new CommentReplyConfiguration());
+        //modelBuilder.ApplyConfiguration(new PostConfiguration());
+        //modelBuilder.ApplyConfiguration(new PostCategoryConfiguration());
+        //modelBuilder.ApplyConfiguration(new PostExtensionConfiguration());
+        //modelBuilder.ApplyConfiguration(new LocalAccountConfiguration());
+        //modelBuilder.ApplyConfiguration(new PingbackConfiguration());
+        //modelBuilder.ApplyConfiguration(new BlogThemeConfiguration());
+        //modelBuilder.ApplyConfiguration(new BlogAssetConfiguration());
+        //modelBuilder.ApplyConfiguration(new BlogConfigurationConfiguration());
+        //modelBuilder.ApplyConfiguration(new PageConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
