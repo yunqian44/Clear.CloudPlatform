9cf7d631c511aaae208c95fff5b85d10c77455e7
yunqian44 <yunqian8@live.com>
2022-03-09 15:41:46 +0000

    Add DbContext

--- src/Data/Entities/SubMenuEntity.cs
+++ src/Data/Entities/SubMenuEntity.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Data.Entities
+namespace Clear.CloudPlatform.Data.Entities;
+
+public class SubMenuEntity
 {
-    public class SubMenuEntity
-    {
-    }
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Url { get; set; }
+
+    public bool IsOpenInNewTab { get; set; }
+
+    public Guid MenuId { get; set; }
+
+    public virtual MenuEntity Menu { get; set; }
 }
---
+++ src/Data/ToolsBlockDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clear.CloudPlatform.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clear.CloudPlatform.Data;
+
+public class ToolsBlockDbContext : DbContext
+{
+    public ToolsBlockDbContext()
+    {
+    }
+
+    public ToolsBlockDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<MenuEntity> Menu { get; set; }
+    public virtual DbSet<SubMenuEntity> SubMenu { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // base.OnModelCreating(modelBuilder);
+        //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        //modelBuilder.ApplyConfiguration(new TagConfiguration());
+        //modelBuilder.ApplyConfiguration(new FriendLinkConfiguration());
+        //modelBuilder.ApplyConfiguration(new MenuConfiguration());
+        //modelBuilder.ApplyConfiguration(new SubMenuConfiguration());
+        //modelBuilder.ApplyConfiguration(new BlogConfigurationConfiguration());
+
+        //modelBuilder
+        //    .Entity<PostEntity>()
+        //    .HasMany(p => p.Tags)
+        //    .WithMany(p => p.Posts)
+        //    .UsingEntity<PostTagEntity>(
+        //        j => j
+        //            .HasOne(pt => pt.Tag)
+        //            .WithMany()
+        //            .HasForeignKey(pt => pt.TagId),
+        //        j => j
+        //            .HasOne(pt => pt.Post)
+        //            .WithMany()
+        //            .HasForeignKey(pt => pt.PostId));
+    }
+}
