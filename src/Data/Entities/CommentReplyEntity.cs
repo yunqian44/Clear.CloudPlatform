fb895ef48e29e518afac6fb598f35e1f97145561
yunqian44 <yunqian8@live.com>
2022-11-02 23:00:19 +0800

    modify init data error

--- src/Data/Entities/CommentReplyEntity.cs
+++ src/Data/Entities/CommentReplyEntity.cs
@@ -8,4 +8,12 @@ namespace Clear.CloudPlatform.Data.Entities;
 
 public class CommentReplyEntity
 {
+    public Guid Id { get; set; }
+    public string ReplyContent { get; set; }
+    public DateTime CreateTimeUtc { get; set; }
+    public Guid? CommentId { get; set; }
+
+    public virtual ToolsBlockEntity Comment { get; set; }
 }
+
+
